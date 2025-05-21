e05b48fe54bcd5e177945bfd4a9b97b5fe589607
yadhuks42 <yadhuks42@gmail.com>
2025-05-21 15:48:04 +0000

    Basic logic and webpack settings

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
 function menu(){
     const content = document.querySelector(`#content`);
     const para = document.createElement(`h1`);
-    para.innerText = "MENU PAGE";
+    para.innerText = "MENU PAGESSS";
     content.append(para);
 }
 
